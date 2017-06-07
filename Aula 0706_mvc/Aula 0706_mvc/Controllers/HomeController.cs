3cc84c486966e16b2a0a80513cdde7507e5fea16
RodrigoMirandaViana <rodrigodemv@gmail.com>
2017-06-07 10:30:47 -0300

    about, index

---
+++ Aula 0706_mvc/Aula 0706_mvc/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Aula_0706_mvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Nome = "Rodrigo";
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+            ViewBag.Nome = "Rodrigo";
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
